0cbf62493dff1505a509c9ef6fd36c4793474efa
Erik <erikgassler@live.com>
2021-05-22 05:53:58 -0700

    Remove Auth code - doesn't meet personal standards

--- Demo/Server/Startup.cs
+++ Demo/Server/Startup.cs
@@ -1,14 +1,8 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Identity.Web;
-using System.Linq;
 
 namespace Demo.Server
 {
@@ -25,9 +19,6 @@ namespace Demo.Server
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
-
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 		}
@@ -52,9 +43,6 @@ namespace Demo.Server
 			app.UseStaticFiles();
 
 			app.UseRouting();
-
-			app.UseAuthentication();
-			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
